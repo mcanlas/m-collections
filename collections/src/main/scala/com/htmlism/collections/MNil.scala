bf1537bc0a2eedb9c68ba334261e5931a9756c4e
Mark Canlas <github@htmlism.com>
2014-08-25 21:44:57 -0400

    drop useless overrides

--- collections/src/main/scala/com/htmlism/collections/MNil.scala
+++ collections/src/main/scala/com/htmlism/collections/MNil.scala
@@ -3,6 +3,6 @@ package com.htmlism.collections
 object MNil extends MListLike[Nothing] {
   def foreach[B](f: Nothing => B) = ()
 
-  override def head = throw new NoSuchElementException("an empty list does not have a head")
-  override def tail = throw new UnsupportedOperationException("an empty list does not have a tail")
+  def head = throw new NoSuchElementException("an empty list does not have a head")
+  def tail = throw new UnsupportedOperationException("an empty list does not have a tail")
 }
