bf362727a9414fddc9c1e979153658d10a2d7f96
Mark Canlas <github@htmlism.com>
2015-05-22 20:42:26 -0400

    added stub implementation

--- collections/src/main/scala/com/htmlism/collections/CollectionFactory.scala
+++ collections/src/main/scala/com/htmlism/collections/CollectionFactory.scala
@@ -3,7 +3,7 @@ package com.htmlism.collections
 import scala.language.higherKinds
 
 trait CollectionFactory[CC[X]] {
-  def apply[A](elements: A*): CC[A]
+  def apply[A](elements: A*): CC[A] = ???
 
 //  def empty: A
 }
