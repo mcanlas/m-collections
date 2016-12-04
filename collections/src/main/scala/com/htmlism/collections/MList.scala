e067bf1ba0146f177d1b66d05ccf164e9e235e39
Mark Canlas <github@htmlism.com>
2016-12-04 06:47:14 +0100

    sealed trait

--- collections/src/main/scala/com/htmlism/collections/MList.scala
+++ collections/src/main/scala/com/htmlism/collections/MList.scala
@@ -6,9 +6,35 @@ object MList extends CollectionFactory[MList] {
   def builder[A]: ListBuilder[A] = new ListBuilder[A]
 }
 
-trait MList[+A] extends LinearSeq[A] {
+sealed trait MList[+A] extends LinearSeq[A] {
   def head: A
   def tail: MList[A]
 
   def ::[B >: A](x: B): MList[B] = new NonEmptyList(x, this)
+}
+
+object MNil extends MList[Nothing] {
+  def iterator: Iterator[Nothing] = Iterator.empty // TODO use custom iterator trait
+
+  def head = throw new NoSuchElementException("an empty list does not have a head")
+  def tail = throw new UnsupportedOperationException("an empty list does not have a tail")
+}
+
+class NonEmptyList[+A](val head: A, val tail: MList[A]) extends MList[A] {
+  private val self = this
+
+  def iterator: Iterator[A] = new Iterator[A] { // TODO use custom iterator trait
+    var remainder = self : MList[A]
+
+    def hasNext: Boolean = remainder match {
+      case _: NonEmptyList[_] => true
+      case    MNil     => false
+    }
+
+    def next() = {
+      val result = remainder.head
+      remainder = remainder.tail
+      result
+    }
+  }
 }
--- collections/src/main/scala/com/htmlism/collections/MNil.scala
+++
@@ -1,8 +0,0 @@
-package com.htmlism.collections
-
-object MNil extends MList[Nothing] {
-  def iterator = Iterator.empty // TODO use custom iterator trait
-
-  def head = throw new NoSuchElementException("an empty list does not have a head")
-  def tail = throw new UnsupportedOperationException("an empty list does not have a tail")
-}
