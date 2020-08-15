5b508e44844a06b9fa1eff82b970f91070dfd453
Mark Canlas <github@htmlism.com>
2020-08-15 04:00:30 -0400

    fmt

--- collections/src/main/scala/com/htmlism/collections/MVector.scala
+++ collections/src/main/scala/com/htmlism/collections/MVector.scala
@@ -7,16 +7,17 @@ object MVector extends CollectionFactory[MVector] {
 }
 
 class MVector[A](values: Array[AnyRef]) extends MIndexedSeq[A] {
-  def iterator = new Iterator[A] { // TODO use custom iterator trait
-    var i = 0
+  def iterator =
+    new Iterator[A] { // TODO use custom iterator trait
+      var i = 0
 
-    def hasNext: Boolean = i < values.length
+      def hasNext: Boolean = i < values.length
 
-    def next(): A = {
-      val v = values(i).asInstanceOf[A]
-      i += 1
+      def next(): A = {
+        val v = values(i).asInstanceOf[A]
+        i += 1
 
-      v
+        v
+      }
     }
-  }
 }
